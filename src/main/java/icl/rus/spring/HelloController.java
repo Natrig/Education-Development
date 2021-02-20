044839d4ca0b5f79230bb609ce94577b65ce2665
Natrig <rustem2301@inbox.ru>
2021-02-20 16:19:09 +0300

    Initial commit

---
+++ src/main/java/icl/rus/spring/HelloController.java
@@ -0,0 +1,17 @@
+package icl.rus.spring;
+
+import icl.rus.spring.base.Sections;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@RequestMapping(Sections.REST)
+public class HelloController {
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello, the time at the server is now " + new Date() + "\n";
+    }
+}
